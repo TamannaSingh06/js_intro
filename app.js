24704a2065091e8f5a8a1b8c94b91058c4e59801
TamannaSingh06 <singhtanu6699@gmail.com>
2024-04-28 23:21:45 +0530

    variables and constants

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log("Hello!");
+
+var old = 234;//old way of variable initialization
+console.log(old);
+
+let myUsn = 153;//new way of variable initialization
+console.log(myUsn);
+
+myUsn = 151;//value of the variable can be changed when let is used
+console.log(old,myUsn);
+
+const pi = 3.14;//constant initialization
+console.log("The value of pi is ",pi);
+
