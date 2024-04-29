c7f6023cb91dece9956cdd0430786057035c2739
TamannaSingh06 <singhtanu6699@gmail.com>
2024-04-30 01:58:58 +0530

    Strings and string methods

--- app.js
+++ app.js
@@ -1,14 +1,47 @@
-console.log("Hello!");
+//Datatypes Number, string, Boolean, null, undefined, object, symbol
 
-var old = 234;//old way of variable initialization
-console.log(old);
+//Strings
+let username = "tamanna.singh" ;
+let domain = "@gmail.com";
+console.log(username,domain);
+let email = username + domain;//concatenation example
+console.log(email);
 
-let myUsn = 153;//new way of variable initialization
-console.log(myUsn);
+//string concatenation
+let verb = 'Learning';
+let noun = 'Javascript';
 
-myUsn = 151;//value of the variable can be changed when let is used
-console.log(old,myUsn);
+let activity = verb +' '+ noun;
+console.log(activity);
 
-const pi = 3.14;//constant initialization
-console.log("The value of pi is ",pi);
+//fetching characters from string via indexing
+console.log(activity[0]);
+console.log(email[2]);
+
+//string length(property: something it possesses)
+console.log(email.length);
+console.log(activity.length);
+
+//srting methods
+console.log(activity.toLowerCase());
+let caps = email.toUpperCase();
+console.log(caps);
+let index = caps.indexOf('@');
+console.log(index);
+
+//Common string methods
+let res = activity.lastIndexOf('a');
+console.log(res);
+
+let res1 = activity.replace('a','o');
+console.log(res1);
+
+let res2 = activity.substring(3,6);
+console.log(res2);
+
+let res3 = activity.slice(8,15);
+console.log(res3);
+
+let res4 = activity.split("n");
+console.log(res4);
 
